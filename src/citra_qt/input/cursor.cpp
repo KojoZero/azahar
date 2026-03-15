6536d51a24b34a9461c653163e9f2621e4676be7
KojoZero <kwaku.kiriguya@gmail.com>
2026-03-15 01:53:08 -0700

    started cursor class

---
+++ src/citra_qt/input/cursor.cpp
@@ -0,0 +1,73 @@
+#include "citra_qt/input/cursor.h"
+#include <cmath>
+#include <algorithm>
+
+
+void Cursor::Restrict(){
+    xPos = std::clamp(xPos, 0.0f, 319.0f);
+    yPos = std::clamp(yPos, 0.0f, 239.0f);
+
+}
+
+void Cursor::Move(float deltaX, float deltaY){
+    xPos += deltaX;
+    yPos += deltaY;
+}
+
+void Cursor::UpdateTouchPosition(){
+    xTouchPos = std::floor(xPos);
+    yTouchPos = std::floor(yPos);
+}
+
+void Cursor::UpdateRenderPosition(int minX, int minY, int maxX, int maxY){
+    pixelWidth = (maxX - minX)/320.0f; //Calculate how wide a 3ds pixel is in terms of current layout
+    pixelHeight = (maxY - minY)/240.0f; //Calculate how tall a 3ds pixel is in terms of current layout
+    xRenderPos = (xTouchPos*pixelWidth)+minX;
+    yRenderPos = (yTouchPos*pixelHeight)+minY;
+}
+
+void Cursor::Render(){
+    // Center->Left->Up->Up->Draw(B)->Right->Draw(B)->Right->Draw(B)
+    Cursor::DrawBlackPixel(xRenderPos-(1*pixelWidth), yRenderPos+(2*pixelHeight));
+    Cursor::DrawBlackPixel(xRenderPos-(0*pixelWidth), yRenderPos+(2*pixelHeight));
+    Cursor::DrawBlackPixel(xRenderPos+(1*pixelWidth), yRenderPos+(2*pixelHeight));
+
+    // Center->Up->Left->Left->Draw(B)->Right->Draw(W)->Right->Draw(W)->Right->Draw(W)->Right->Draw(B)
+    Cursor::DrawBlackPixel(xRenderPos-(2*pixelWidth), yRenderPos+(1*pixelHeight));
+    Cursor::DrawWhitePixel(xRenderPos-(1*pixelWidth), yRenderPos+(1*pixelHeight));
+    Cursor::DrawWhitePixel(xRenderPos-(0*pixelWidth), yRenderPos+(1*pixelHeight));
+    Cursor::DrawWhitePixel(xRenderPos+(1*pixelWidth), yRenderPos+(1*pixelHeight));
+    Cursor::DrawBlackPixel(xRenderPos+(2*pixelWidth), yRenderPos+(1*pixelHeight));
+
+    // Center->Left->Left->Draw(B)->Right->Draw(W)->Right->Draw(W)->Right->Draw(W)->Right->Draw(B)
+    Cursor::DrawBlackPixel(xRenderPos-(2*pixelWidth), yRenderPos);
+    Cursor::DrawWhitePixel(xRenderPos-(1*pixelWidth), yRenderPos);
+    Cursor::DrawWhitePixel(xRenderPos-(0*pixelWidth), yRenderPos);
+    Cursor::DrawWhitePixel(xRenderPos+(1*pixelWidth), yRenderPos);
+    Cursor::DrawBlackPixel(xRenderPos+(2*pixelWidth), yRenderPos);
+
+    // Center->Down->Left->Left->Draw(B)->Right->Draw(W)->Right->Draw(W)->Right->Draw(W)->Right->Draw(B)
+    Cursor::DrawBlackPixel(xRenderPos-(2*pixelWidth), yRenderPos-(1*pixelHeight));
+    Cursor::DrawWhitePixel(xRenderPos-(1*pixelWidth), yRenderPos-(1*pixelHeight));
+    Cursor::DrawWhitePixel(xRenderPos-(0*pixelWidth), yRenderPos-(1*pixelHeight));
+    Cursor::DrawWhitePixel(xRenderPos+(1*pixelWidth), yRenderPos-(1*pixelHeight));
+    Cursor::DrawBlackPixel(xRenderPos+(2*pixelWidth), yRenderPos-(1*pixelHeight));
+
+    // Center->Left->Down->Down->Draw(B)->Right->Draw(B)->Right->Draw(B)
+    Cursor::DrawBlackPixel(xRenderPos-(1*pixelWidth), yRenderPos-(2*pixelHeight));
+    Cursor::DrawBlackPixel(xRenderPos-(0*pixelWidth), yRenderPos-(2*pixelHeight));
+    Cursor::DrawBlackPixel(xRenderPos+(1*pixelWidth), yRenderPos-(2*pixelHeight));
+}
+
+void DrawBlackPixel(float x, float y){
+//painter.fillRect((int) x,(int)y,(int) pixelWidth, (int)pixelHeight, Qt::black)
+}
+
+void DrawWhitePixel(float x, float y){
+ //painter.fillRect((int) x,(int)y,(int) pixelWidth, (int)pixelHeight, Qt::white)
+}
+
+
+bool Cursor::GetIsPressed(){
+
+}
---
+++ src/citra_qt/input/cursor.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <QPainter>
+#include <QPixmap>
+
+class Cursor {
+public:
+    Cursor();
+    ~Cursor();
+    //Clamp the cursor position within touchscreen boundaries
+    void Restrict();
+
+    //Move the cursor in terms of fractions 3ds coordinates
+    void Move(float deltaX, float deltaY);
+
+    //Calculate the render position of cursor for current layout
+    void UpdateRenderPosition(int minX, int minY, int maxX, int maxY);
+
+    //Calculate the touch position of cursor in 3ds coordinates (integer)
+    void UpdateTouchPosition();
+
+    //Draw the cursor on screen
+    void Render();
+
+    void DrawBlackPixel(float x, float y);
+
+    void DrawWhitePixel(float x, float y);
+
+    //Return whether touchscreen is being pressed
+    bool GetIsPressed();
+
+private:
+    //Cursor Coordinates in terms of fractional 3ds coordinates
+    float xPos;
+    float yPos;
+
+    //Cursor Coordinates mapped to 3ds internal coordinates
+    int xTouchPos;
+    int yTouchPos;
+
+    //Cursor Coordinates mapped to layout coordinates and snapped to grid for draw. The value represents the bottom left of the rendered center pixel
+    float xRenderPos;
+    float yRenderPos;
+
+    float pixelHeight;
+    float pixelWidth;
+
+    //Whether touchscreen is pressed
+    bool isPressed;
+};
